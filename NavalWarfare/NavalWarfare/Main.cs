670dceb015d030b1937ee8ff0a31350ff6e06f52
teofurlan <teofurlan03@gmail.com>
2023-11-06 17:40:24 -0300

    Arrancamo

---
+++ NavalWarfare/NavalWarfare/Main.cs
@@ -0,0 +1,10 @@
+namespace NavalWarfare
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- NavalWarfare/NavalWarfare/Program.cs
+++ NavalWarfare/NavalWarfare/Program.cs
@@ -1,12 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
 namespace NavalWarfare
 {
-    static class Program
+    internal static class Program
     {
         /// <summary>
         ///  The main entry point for the application.
@@ -17,7 +11,7 @@ namespace NavalWarfare
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new Main());
         }
     }
 }
