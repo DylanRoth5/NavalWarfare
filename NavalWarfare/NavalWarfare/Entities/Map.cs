8fba288c3bd84c869d0c48c89b29ed5f8aec3f7a
DylanRoth5 <130003124+DylanRoth5@users.noreply.github.com>
2023-11-11 09:29:09 -0300

    map

--- NavalWarfare/NavalWarfare/Entities/Map.cs
+++ NavalWarfare/NavalWarfare/Entities/Map.cs
@@ -1,3 +1,4 @@
+using System.Drawing;
 using static System.ConsoleColor;
 using static NavalWarfare.HelperSeal.Tools;
 
@@ -6,74 +7,19 @@ namespace NavalWarfare.Entities;
 public class Map
 {
     public int Id;
-    public static string Spot = "  \u25a0";
-    public string[,] Matrix = new string[10,10];
-    public Player Gamer;
+    public readonly static int Spot = 0;
+    public int[,] Matrix;
+    public int Size;
 
-    public Map(int id,Player gamer)
+    public Map(int id,int size)
     {
         Id = id;
-        Gamer = gamer;
-        CleanMap();
+        Size = size;
+        Matrix = new int[Size, Size];
     }
-    public Map(Player gamer)
+    public Map(int size)
     {
-        Gamer = gamer;
-        CleanMap();
-    }
-
-    public void CleanMap()
-    {
-        for (int i = 0; i < 10; i++)
-        {
-            for (int j = 0; j < 10; j++)
-            {
-                Matrix[i, j] = Spot;
-            }
-        }
-    }
-
-    public void ShowMap(bool ItsMe)
-    {
-        for (var i = 0; i < 10; i++)
-        {
-            for (var j = 0; j < 10; j++)
-            {
-                if (Matrix[i,j]==Missile.Skin2)
-                {
-                    Write(Matrix[i,j],Red);
-                }else if (Matrix[i,j]==Ship.Skin2 && ItsMe)
-                {
-                    Write(Matrix[i,j],DarkMagenta);
-                }else Write(Spot,Blue);
-            }
-            WriteLine();
-        }
-    }
-    public void SelectInMap(bool ItsMe,int x,int y)
-    {
-        for (var i = 0; i < 10; i++)
-        {
-            for (var j = 0; j < 10; j++)
-            {
-                if (x==i && y==j) Flip();
-                if (Matrix[i, j] == Missile.Skin2)
-                    Write(Matrix[i, j], Red);
-                else if (Matrix[i, j] == Ship.Skin2 && ItsMe)
-                    Write(Matrix[i, j], DarkMagenta);
-                else Write(Spot,Blue);
-                if (x==i && y==j) Flip();
-            }
-            WriteLine();
-        }
-    }
-
-    public bool HasShips()
-    {
-        for (var i = 0; i < 10; i++)
-        for (var j = 0; j < 10; j++)
-            if (Matrix[i, j] == Ship.Skin2)
-                return true;
-        return false;
+        Size = size;
+        Matrix = new int[Size, Size];
     }
 }
