4cdbaa33b9b88beb6f644028bca3b14ab17ce27c
DylanRoth5 <130003124+DylanRoth5@users.noreply.github.com>
2023-11-11 09:34:44 -0300

    Coop from Enzo added

--- NavalWarfare/NavalWarfare/gCoop.Designer.cs
+++ NavalWarfare/NavalWarfare/gCoop.Designer.cs
@@ -28,14 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             btCoopClose = new Button();
             panel1 = new Panel();
-            btCoopStart = new Button();
+            d10 = new Button();
+            d9 = new Button();
+            d8 = new Button();
+            d7 = new Button();
+            d6 = new Button();
+            d5 = new Button();
+            d4 = new Button();
+            d3 = new Button();
+            d2 = new Button();
+            d1 = new Button();
+            b10 = new Button();
+            b9 = new Button();
+            b8 = new Button();
+            b7 = new Button();
+            b6 = new Button();
+            b5 = new Button();
+            b4 = new Button();
+            b3 = new Button();
+            c10 = new Button();
+            c9 = new Button();
+            c8 = new Button();
+            c7 = new Button();
+            c6 = new Button();
+            c5 = new Button();
+            c4 = new Button();
+            c3 = new Button();
+            c2 = new Button();
+            c1 = new Button();
+            b2 = new Button();
+            b1 = new Button();
+            a10 = new Button();
+            a9 = new Button();
+            a8 = new Button();
+            a7 = new Button();
+            a6 = new Button();
+            a5 = new Button();
+            a4 = new Button();
+            a3 = new Button();
+            a2 = new Button();
+            a1 = new Button();
+            btStartCoop = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // btCoopClose
             // 
-            btCoopClose.Location = new Point(23, 30);
+            btCoopClose.Location = new Point(12, 12);
             btCoopClose.Name = "btCoopClose";
             btCoopClose.Size = new Size(94, 29);
             btCoopClose.TabIndex = 0;
@@ -46,31 +89,476 @@
             // panel1
             // 
             panel1.BackColor = Color.CornflowerBlue;
-            panel1.Location = new Point(174, 37);
+            panel1.Controls.Add(d10);
+            panel1.Controls.Add(d9);
+            panel1.Controls.Add(d8);
+            panel1.Controls.Add(d7);
+            panel1.Controls.Add(d6);
+            panel1.Controls.Add(d5);
+            panel1.Controls.Add(d4);
+            panel1.Controls.Add(d3);
+            panel1.Controls.Add(d2);
+            panel1.Controls.Add(d1);
+            panel1.Controls.Add(b10);
+            panel1.Controls.Add(b9);
+            panel1.Controls.Add(b8);
+            panel1.Controls.Add(b7);
+            panel1.Controls.Add(b6);
+            panel1.Controls.Add(b5);
+            panel1.Controls.Add(b4);
+            panel1.Controls.Add(b3);
+            panel1.Controls.Add(c10);
+            panel1.Controls.Add(c9);
+            panel1.Controls.Add(c8);
+            panel1.Controls.Add(c7);
+            panel1.Controls.Add(c6);
+            panel1.Controls.Add(c5);
+            panel1.Controls.Add(c4);
+            panel1.Controls.Add(c3);
+            panel1.Controls.Add(c2);
+            panel1.Controls.Add(c1);
+            panel1.Controls.Add(b2);
+            panel1.Controls.Add(b1);
+            panel1.Controls.Add(a10);
+            panel1.Controls.Add(a9);
+            panel1.Controls.Add(a8);
+            panel1.Controls.Add(a7);
+            panel1.Controls.Add(a6);
+            panel1.Controls.Add(a5);
+            panel1.Controls.Add(a4);
+            panel1.Controls.Add(a3);
+            panel1.Controls.Add(a2);
+            panel1.Controls.Add(a1);
+            panel1.Location = new Point(135, 40);
             panel1.Name = "panel1";
             panel1.Size = new Size(1068, 508);
             panel1.TabIndex = 1;
             // 
-            // btCoopStart
+            // d10
             // 
-            btCoopStart.Location = new Point(23, 111);
-            btCoopStart.Name = "btCoopStart";
-            btCoopStart.Size = new Size(94, 29);
-            btCoopStart.TabIndex = 2;
-            btCoopStart.Text = "START";
-            btCoopStart.UseVisualStyleBackColor = true;
-            btCoopStart.Click += btCoopStart_Click;
+            d10.Location = new Point(921, 338);
+            d10.Name = "d10";
+            d10.Size = new Size(90, 90);
+            d10.TabIndex = 39;
+            d10.UseVisualStyleBackColor = true;
+            d10.Visible = false;
+            d10.Click += BotonPosicion_Click;
+            // 
+            // d9
+            // 
+            d9.Location = new Point(825, 338);
+            d9.Name = "d9";
+            d9.Size = new Size(90, 90);
+            d9.TabIndex = 38;
+            d9.UseVisualStyleBackColor = true;
+            d9.Visible = false;
+            d9.Click += BotonPosicion_Click;
+            // 
+            // d8
+            // 
+            d8.Location = new Point(729, 338);
+            d8.Name = "d8";
+            d8.Size = new Size(90, 90);
+            d8.TabIndex = 37;
+            d8.UseVisualStyleBackColor = true;
+            d8.Visible = false;
+            d8.Click += BotonPosicion_Click;
+            // 
+            // d7
+            // 
+            d7.Location = new Point(633, 338);
+            d7.Name = "d7";
+            d7.Size = new Size(90, 90);
+            d7.TabIndex = 36;
+            d7.UseVisualStyleBackColor = true;
+            d7.Visible = false;
+            d7.Click += BotonPosicion_Click;
+            // 
+            // d6
+            // 
+            d6.Location = new Point(537, 338);
+            d6.Name = "d6";
+            d6.Size = new Size(90, 90);
+            d6.TabIndex = 35;
+            d6.UseVisualStyleBackColor = true;
+            d6.Visible = false;
+            d6.Click += BotonPosicion_Click;
+            // 
+            // d5
+            // 
+            d5.Location = new Point(441, 338);
+            d5.Name = "d5";
+            d5.Size = new Size(90, 90);
+            d5.TabIndex = 34;
+            d5.UseVisualStyleBackColor = true;
+            d5.Visible = false;
+            d5.Click += BotonPosicion_Click;
+            // 
+            // d4
+            // 
+            d4.Location = new Point(345, 338);
+            d4.Name = "d4";
+            d4.Size = new Size(90, 90);
+            d4.TabIndex = 33;
+            d4.UseVisualStyleBackColor = true;
+            d4.Visible = false;
+            d4.Click += BotonPosicion_Click;
+            // 
+            // d3
+            // 
+            d3.Location = new Point(249, 338);
+            d3.Name = "d3";
+            d3.Size = new Size(90, 90);
+            d3.TabIndex = 32;
+            d3.UseVisualStyleBackColor = true;
+            d3.Visible = false;
+            d3.Click += BotonPosicion_Click;
+            // 
+            // d2
+            // 
+            d2.Location = new Point(153, 338);
+            d2.Name = "d2";
+            d2.Size = new Size(90, 90);
+            d2.TabIndex = 31;
+            d2.UseVisualStyleBackColor = true;
+            d2.Visible = false;
+            d2.Click += BotonPosicion_Click;
+            // 
+            // d1
+            // 
+            d1.Location = new Point(57, 338);
+            d1.Name = "d1";
+            d1.Size = new Size(90, 90);
+            d1.TabIndex = 30;
+            d1.UseVisualStyleBackColor = true;
+            d1.Visible = false;
+            d1.Click += BotonPosicion_Click;
+            // 
+            // b10
+            // 
+            b10.Location = new Point(921, 146);
+            b10.Name = "b10";
+            b10.Size = new Size(90, 90);
+            b10.TabIndex = 29;
+            b10.UseVisualStyleBackColor = true;
+            b10.Visible = false;
+            b10.Click += BotonPosicion_Click;
+            // 
+            // b9
+            // 
+            b9.Location = new Point(825, 146);
+            b9.Name = "b9";
+            b9.Size = new Size(90, 90);
+            b9.TabIndex = 28;
+            b9.UseVisualStyleBackColor = true;
+            b9.Visible = false;
+            b9.Click += BotonPosicion_Click;
+            // 
+            // b8
+            // 
+            b8.Location = new Point(729, 146);
+            b8.Name = "b8";
+            b8.Size = new Size(90, 90);
+            b8.TabIndex = 27;
+            b8.UseVisualStyleBackColor = true;
+            b8.Visible = false;
+            b8.Click += BotonPosicion_Click;
+            // 
+            // b7
+            // 
+            b7.Location = new Point(633, 146);
+            b7.Name = "b7";
+            b7.Size = new Size(90, 90);
+            b7.TabIndex = 26;
+            b7.UseVisualStyleBackColor = true;
+            b7.Visible = false;
+            b7.Click += BotonPosicion_Click;
+            // 
+            // b6
+            // 
+            b6.Location = new Point(537, 146);
+            b6.Name = "b6";
+            b6.Size = new Size(90, 90);
+            b6.TabIndex = 25;
+            b6.UseVisualStyleBackColor = true;
+            b6.Visible = false;
+            b6.Click += BotonPosicion_Click;
+            // 
+            // b5
+            // 
+            b5.Location = new Point(441, 146);
+            b5.Name = "b5";
+            b5.Size = new Size(90, 90);
+            b5.TabIndex = 24;
+            b5.UseVisualStyleBackColor = true;
+            b5.Visible = false;
+            b5.Click += BotonPosicion_Click;
+            // 
+            // b4
+            // 
+            b4.Location = new Point(345, 146);
+            b4.Name = "b4";
+            b4.Size = new Size(90, 90);
+            b4.TabIndex = 23;
+            b4.UseVisualStyleBackColor = true;
+            b4.Visible = false;
+            b4.Click += BotonPosicion_Click;
+            // 
+            // b3
+            // 
+            b3.Location = new Point(249, 146);
+            b3.Name = "b3";
+            b3.Size = new Size(90, 90);
+            b3.TabIndex = 22;
+            b3.UseVisualStyleBackColor = true;
+            b3.Visible = false;
+            b3.Click += BotonPosicion_Click;
+            // 
+            // c10
+            // 
+            c10.Location = new Point(921, 242);
+            c10.Name = "c10";
+            c10.Size = new Size(90, 90);
+            c10.TabIndex = 21;
+            c10.UseVisualStyleBackColor = true;
+            c10.Visible = false;
+            c10.Click += BotonPosicion_Click;
+            // 
+            // c9
+            // 
+            c9.Location = new Point(825, 242);
+            c9.Name = "c9";
+            c9.Size = new Size(90, 90);
+            c9.TabIndex = 20;
+            c9.UseVisualStyleBackColor = true;
+            c9.Visible = false;
+            c9.Click += BotonPosicion_Click;
+            // 
+            // c8
+            // 
+            c8.Location = new Point(729, 242);
+            c8.Name = "c8";
+            c8.Size = new Size(90, 90);
+            c8.TabIndex = 19;
+            c8.UseVisualStyleBackColor = true;
+            c8.Visible = false;
+            c8.Click += BotonPosicion_Click;
+            // 
+            // c7
+            // 
+            c7.Location = new Point(633, 242);
+            c7.Name = "c7";
+            c7.Size = new Size(90, 90);
+            c7.TabIndex = 18;
+            c7.UseVisualStyleBackColor = true;
+            c7.Visible = false;
+            c7.Click += BotonPosicion_Click;
+            // 
+            // c6
+            // 
+            c6.Location = new Point(537, 242);
+            c6.Name = "c6";
+            c6.Size = new Size(90, 90);
+            c6.TabIndex = 17;
+            c6.UseVisualStyleBackColor = true;
+            c6.Visible = false;
+            c6.Click += BotonPosicion_Click;
+            // 
+            // c5
+            // 
+            c5.Location = new Point(441, 242);
+            c5.Name = "c5";
+            c5.Size = new Size(90, 90);
+            c5.TabIndex = 16;
+            c5.UseVisualStyleBackColor = true;
+            c5.Visible = false;
+            c5.Click += BotonPosicion_Click;
+            // 
+            // c4
+            // 
+            c4.Location = new Point(345, 242);
+            c4.Name = "c4";
+            c4.Size = new Size(90, 90);
+            c4.TabIndex = 15;
+            c4.UseVisualStyleBackColor = true;
+            c4.Visible = false;
+            c4.Click += BotonPosicion_Click;
+            // 
+            // c3
+            // 
+            c3.Location = new Point(249, 242);
+            c3.Name = "c3";
+            c3.Size = new Size(90, 90);
+            c3.TabIndex = 14;
+            c3.UseVisualStyleBackColor = true;
+            c3.Visible = false;
+            c3.Click += BotonPosicion_Click;
+            // 
+            // c2
+            // 
+            c2.Location = new Point(153, 242);
+            c2.Name = "c2";
+            c2.Size = new Size(90, 90);
+            c2.TabIndex = 13;
+            c2.UseVisualStyleBackColor = true;
+            c2.Visible = false;
+            c2.Click += BotonPosicion_Click;
+            // 
+            // c1
+            // 
+            c1.Location = new Point(57, 242);
+            c1.Name = "c1";
+            c1.Size = new Size(90, 90);
+            c1.TabIndex = 12;
+            c1.UseVisualStyleBackColor = true;
+            c1.Visible = false;
+            c1.Click += BotonPosicion_Click;
+            // 
+            // b2
+            // 
+            b2.Location = new Point(153, 146);
+            b2.Name = "b2";
+            b2.Size = new Size(90, 90);
+            b2.TabIndex = 11;
+            b2.UseVisualStyleBackColor = true;
+            b2.Visible = false;
+            b2.Click += BotonPosicion_Click;
+            // 
+            // b1
+            // 
+            b1.Location = new Point(57, 146);
+            b1.Name = "b1";
+            b1.Size = new Size(90, 90);
+            b1.TabIndex = 10;
+            b1.UseVisualStyleBackColor = true;
+            b1.Visible = false;
+            b1.Click += BotonPosicion_Click;
+            // 
+            // a10
+            // 
+            a10.Location = new Point(921, 50);
+            a10.Name = "a10";
+            a10.Size = new Size(90, 90);
+            a10.TabIndex = 9;
+            a10.UseVisualStyleBackColor = true;
+            a10.Visible = false;
+            a10.Click += BotonPosicion_Click;
+            // 
+            // a9
+            // 
+            a9.Location = new Point(825, 50);
+            a9.Name = "a9";
+            a9.Size = new Size(90, 90);
+            a9.TabIndex = 8;
+            a9.UseVisualStyleBackColor = true;
+            a9.Visible = false;
+            a9.Click += BotonPosicion_Click;
+            // 
+            // a8
+            // 
+            a8.Location = new Point(729, 50);
+            a8.Name = "a8";
+            a8.Size = new Size(90, 90);
+            a8.TabIndex = 7;
+            a8.UseVisualStyleBackColor = true;
+            a8.Visible = false;
+            a8.Click += BotonPosicion_Click;
+            // 
+            // a7
+            // 
+            a7.Location = new Point(633, 50);
+            a7.Name = "a7";
+            a7.Size = new Size(90, 90);
+            a7.TabIndex = 6;
+            a7.UseVisualStyleBackColor = true;
+            a7.Visible = false;
+            a7.Click += BotonPosicion_Click;
+            // 
+            // a6
+            // 
+            a6.Location = new Point(537, 50);
+            a6.Name = "a6";
+            a6.Size = new Size(90, 90);
+            a6.TabIndex = 5;
+            a6.UseVisualStyleBackColor = true;
+            a6.Visible = false;
+            a6.Click += BotonPosicion_Click;
+            // 
+            // a5
+            // 
+            a5.Location = new Point(441, 50);
+            a5.Name = "a5";
+            a5.Size = new Size(90, 90);
+            a5.TabIndex = 4;
+            a5.UseVisualStyleBackColor = true;
+            a5.Visible = false;
+            a5.Click += BotonPosicion_Click;
+            // 
+            // a4
+            // 
+            a4.Location = new Point(345, 50);
+            a4.Name = "a4";
+            a4.Size = new Size(90, 90);
+            a4.TabIndex = 3;
+            a4.UseVisualStyleBackColor = true;
+            a4.Visible = false;
+            a4.Click += BotonPosicion_Click;
+            // 
+            // a3
+            // 
+            a3.Location = new Point(249, 50);
+            a3.Name = "a3";
+            a3.Size = new Size(90, 90);
+            a3.TabIndex = 2;
+            a3.UseVisualStyleBackColor = true;
+            a3.Visible = false;
+            a3.Click += BotonPosicion_Click;
+            // 
+            // a2
+            // 
+            a2.Location = new Point(153, 50);
+            a2.Name = "a2";
+            a2.Size = new Size(90, 90);
+            a2.TabIndex = 1;
+            a2.UseVisualStyleBackColor = true;
+            a2.Visible = false;
+            a2.Click += BotonPosicion_Click;
+            // 
+            // a1
+            // 
+            a1.Location = new Point(57, 50);
+            a1.Name = "a1";
+            a1.Size = new Size(90, 90);
+            a1.TabIndex = 0;
+            a1.UseVisualStyleBackColor = true;
+            a1.Visible = false;
+            a1.Click += BotonPosicion_Click;
+            // 
+            // btStartCoop
+            // 
+            btStartCoop.Location = new Point(12, 90);
+            btStartCoop.Name = "btStartCoop";
+            btStartCoop.Size = new Size(94, 29);
+            btStartCoop.TabIndex = 2;
+            btStartCoop.Text = "START";
+            btStartCoop.UseVisualStyleBackColor = true;
+            btStartCoop.Click += button2_Click;
+            // 
+            // timer1
+            // 
+            timer1.Interval = 1000;
             // 
             // gCoop
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1264, 578);
-            Controls.Add(btCoopStart);
+            Controls.Add(btStartCoop);
             Controls.Add(panel1);
             Controls.Add(btCoopClose);
             Name = "gCoop";
             Text = "gCoop";
+            panel1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -78,6 +566,47 @@
 
         private Button btCoopClose;
         private Panel panel1;
-        private Button btCoopStart;
+        private Button d10;
+        private Button d9;
+        private Button d8;
+        private Button d7;
+        private Button d6;
+        private Button d5;
+        private Button d4;
+        private Button d3;
+        private Button d2;
+        private Button d1;
+        private Button b10;
+        private Button b9;
+        private Button b8;
+        private Button b7;
+        private Button b6;
+        private Button b5;
+        private Button b4;
+        private Button b3;
+        private Button c10;
+        private Button c9;
+        private Button c8;
+        private Button c7;
+        private Button c6;
+        private Button c5;
+        private Button c4;
+        private Button c3;
+        private Button c2;
+        private Button c1;
+        private Button b2;
+        private Button b1;
+        private Button a10;
+        private Button a9;
+        private Button a8;
+        private Button a7;
+        private Button a6;
+        private Button a5;
+        private Button a4;
+        private Button a3;
+        private Button a2;
+        private Button a1;
+        private Button btStartCoop;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- NavalWarfare/NavalWarfare/gCoop.cs
+++ NavalWarfare/NavalWarfare/gCoop.cs
@@ -12,21 +12,89 @@ namespace NavalWarfare
 {
     public partial class gCoop : Form
     {
+        List<Button> shipPossitionButtons;
+
+        Random r = new Random();
+
+        System.Windows.Forms.Timer timer;
+        TimeSpan tiempoTranscurrido;
+
         public gCoop()
         {
             InitializeComponent();
+            InicializarCronometro();
         }
 
         private void btCoopClose_Click(object sender, EventArgs e)
         {
+            foreach (var button in shipPossitionButtons) { button.Visible = false; }
+            ReiniciarCronometro();
             this.Hide();
             Program.mainMenu.Show();
         }
 
-        private void btCoopStart_Click(object sender, EventArgs e)
+        private void InicializarCronometro()
         {
-            //cree la matriz en el panles con sus respetivos barcos. cuando apretas una posicion te dice si le pegaste a un barco o no
-            // si le pegas a todos los barcos en el mapa ganas el juego
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = 1000; //para que cuente bien los segundos
+            timer.Tick += Timer_Tick;
         }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            //Actualizar la visualización del tiempo si es necesario
+            tiempoTranscurrido = tiempoTranscurrido.Add(TimeSpan.FromSeconds(1));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //lista con todos los botones que forman la matriz de lugares donde hay barcos
+            shipPossitionButtons = new List<Button> { a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10 };
+
+            //asigna a 5 botones el tag 'enemyShip' para que haya un barco en ese boton
+            for (int i = 0; i < 5; i++)
+            {
+                int indiceAleatorio = r.Next(shipPossitionButtons.Count);
+                shipPossitionButtons[indiceAleatorio].Tag = "enemyShip";
+            }
+
+            //los botones estan ocultos antes de clickear start, este foreach los vuelve visibles
+            foreach (var button in shipPossitionButtons) { button.Visible = true; }
+
+            //inicia el cronómetro 
+            tiempoTranscurrido = new TimeSpan();
+            timer.Start();
+        }
+        private void BotonPosicion_Click(object sender, EventArgs e)
+        {
+            //verifica si el botón clicado tiene el tag enemyShip
+            Button botonClicado = sender as Button;
+            if (botonClicado != null && "enemyShip".Equals(botonClicado.Tag))
+            {
+                //realiza las acciones necesarias cuando se clickea un botón con el tag enemyShip
+                MessageBox.Show("¡Encontraste un barco");
+
+                //quita la propiedad para que no se pueda contar más de una vez
+                botonClicado.Tag = null;
+            }
+
+            //devulve true cuando no haya ningun barco con el tag enemyShip y muestra q termino la partida
+            if (!shipPossitionButtons.Any(button => "enemyShip".Equals(button.Tag)))
+            {
+                timer.Stop();
+                MessageBox.Show($"¡Ganaste la partida! Tiempo: {tiempoTranscurrido.ToString(@"mm\:ss")}");
+                foreach (var button in shipPossitionButtons) { button.Visible = false; }
+                this.Hide();
+                Program.mainMenu.Show();
+                ReiniciarCronometro();
+            }
+
+        }
+        private void ReiniciarCronometro()
+        {
+            tiempoTranscurrido = new TimeSpan();
+            timer.Start();
+        }
+
     }
 }
